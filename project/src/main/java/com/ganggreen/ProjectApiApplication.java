e5676199d90dd8f9dd919affe3c894bf324eaa7f
Jason <71078248+JsonMack@users.noreply.github.com>
2022-02-19 22:41:50 -0400

    Updated a few other controllers.

--- project/src/main/java/com/ganggreen/ProjectApiApplication.java
+++ project/src/main/java/com/ganggreen/ProjectApiApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Jason MacKeigan
@@ -12,9 +16,17 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
         DataSourceAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class
 })
+@EnableEurekaClient
 public class ProjectApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApiApplication.class, args);
     }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
--- project/src/main/java/com/ganggreen/controller/ProjectController.java
+++ project/src/main/java/com/ganggreen/controller/ProjectController.java
@@ -1,19 +1,27 @@
 package com.ganggreen.controller;
 
 import com.ganggreen.model.Project;
+import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Jason MacKeigan
  */
 @RestController("/project")
 public class ProjectController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping(path = "/{id}")
     @Operation(
