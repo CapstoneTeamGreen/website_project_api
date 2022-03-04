4f7d807a0ea57e3c594adf341a697253ae08bf47
Jason MacKeigan <w0270109@nscc.ca>
2022-03-04 08:43:00 -0400

    Decoupled service discovery component from project api

--- src/main/java/com/ganggreen/controller/ProjectController.java
+++ src/main/java/com/ganggreen/controller/ProjectController.java
@@ -8,11 +8,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.print.attribute.standard.Media;
+import java.util.List;
 
 /**
  * @author Jason MacKeigan
@@ -23,7 +27,7 @@ public class ProjectController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             description = "Returns the specific project for the given id.",
             responses = @ApiResponse(responseCode = "200",
@@ -32,6 +36,11 @@ public class ProjectController {
     public Project get(@RequestAttribute int id) {
         //TODO remove example
         return new Project("Test!");
+    }
+
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Project> getAll() {
+        return List.of(new Project("Test1"), new Project("Test2"));
     }
 
 }
