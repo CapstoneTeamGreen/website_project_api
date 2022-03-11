87dd70fddca3b752efcc17932c3fcceb88746df0
Jason MacKeigan <w0270109@nscc.ca>
2022-03-11 12:00:01 -0400

    Who knows tbh.

--- src/main/java/com/ganggreen/ProjectApiApplication.java
+++ src/main/java/com/ganggreen/ProjectApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
         HibernateJpaAutoConfiguration.class
 })
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ProjectApiApplication {
 
     public static void main(String[] args) {
